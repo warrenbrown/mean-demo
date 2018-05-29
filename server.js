d5f6df593f00a6484062b736f9efee71ff16bf95
warrenbrown <warren@katapultmedia.com>
2018-05-29 11:22:11 -0500

    Added angular client to project

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const api = require('./routes/api');
 const port = process.env.PORT || 3000;
@@ -17,11 +19,15 @@ mongoose.connect(config.uri, (err) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use('/api', api);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'dist/index.html'));
+});
 
 app.listen(port, function() {
   console.log('Server is running at http://localhost:' + port);
