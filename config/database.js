09481935d212ab9b9a274d7bd9c3095aaa4ea7da
warrenbrown <warren@katapultmedia.com>
2018-05-29 08:33:09 -0500

    added database to server.js

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri: 'mongodb://localhost:27017/mean-demo',
+  db: 'mean-demo'
+}
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const port = 3000
+const port = process.env.PORT || 3000;
+const config = require('./config/database');
+
+//connect to the database
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('unable to connect to database')
+  } else {
+    console.log('Connected to database:' + config.db);
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app');
 });
 
+app.get('/home', (req, res) => {
+  res.send('Welcom to the home page');
+})
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log('Server is running at http://localhost:' + port);
 });
